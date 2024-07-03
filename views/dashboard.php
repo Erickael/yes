03edd47ae45f7794afc644677c3fd397986f020a
Khada <141226750+Erickael@users.noreply.github.com>
2024-07-03 09:03:19 +0300

    Update dashboard.php

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,5 +1,5 @@
 <div id="dashboard" class="pt-4">
-    <h2>Bienvenue sur le tableau de bord de votre banque</h2>
+    <h2>Birnvenue sur le tableau de bord</h2>
     <p>Profitez de nos services bancaires en ligne pour gérer vos finances en toute simplicité et sécurité.</p>
-    <p>Publicité: Découvrez nos nouvelles offres de crédit à taux réduit!</p>
+    <p>Publicité: Lorem ipsum nlef nlave ntsmet de niol nfot</p>
 </div>
